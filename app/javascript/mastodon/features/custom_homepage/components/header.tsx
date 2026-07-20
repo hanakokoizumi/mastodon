4b96ca23ed00d2ed5b5248b8543acf453f7706ee
Hanako <i@hanako.me>
2026-07-20 18:08:32 +0800

    Show a loading state while translating statuses.
    Disable duplicate translate requests, surface a spinner on the translate button, and prefer the SSO login path on the overview header.

--- app/javascript/mastodon/features/custom_homepage/components/header.tsx
+++ app/javascript/mastodon/features/custom_homepage/components/header.tsx
@@ -2,24 +2,31 @@ import { FormattedMessage } from 'react-intl';
 
 import { Link } from 'react-router-dom';
 
-import { domain, sso_redirect } from 'mastodon/initial_state';
+import { domain, sso_redirect, sso_login_path } from 'mastodon/initial_state';
 
 import classes from '../styles.module.scss';
 
-export const Header = () => (
-  <div className={classes.minimalHeader}>
-    <div className={classes.leftSide}>
-      <Link to='/overview'>{domain}</Link>
-    </div>
+export const Header = () => {
+  const ssoCombinedPath = sso_redirect ?? sso_login_path;
 
-    <div className={classes.rightSide}>
-      <a
-        href={sso_redirect ?? '/auth/sign_in'}
-        data-method={sso_redirect ? 'post' : undefined}
-        className='button button-secondary'
-      >
-        <FormattedMessage id='sign_in_banner.sign_in' defaultMessage='Login' />
-      </a>
+  return (
+    <div className={classes.minimalHeader}>
+      <div className={classes.leftSide}>
+        <Link to='/overview'>{domain}</Link>
+      </div>
+
+      <div className={classes.rightSide}>
+        <a
+          href={ssoCombinedPath ?? '/auth/sign_in'}
+          data-method={ssoCombinedPath ? 'post' : undefined}
+          className='button button-secondary'
+        >
+          <FormattedMessage
+            id='sign_in_banner.sign_in'
+            defaultMessage='Login'
+          />
+        </a>
+      </div>
     </div>
-  </div>
-);
+  );
+};
